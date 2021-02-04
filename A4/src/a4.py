0c485d60f9a7923da11158745fca7a8bea710b05
nmolica <60078553+nmolica@users.noreply.github.com>
2021-02-04 15:28:19 -0500

    Ready for testing

--- A4/src/a4.py
+++ A4/src/a4.py
@@ -17,26 +17,26 @@ elif len(sys.argv) > 0:
     host = sys.argv[0]
 
 # Recieve a message from the server by looping on recv
-# until the message constitutes a valid JSON object.
+# until we find a newline. Complain if it's invalid JSON.
 def recieve(sock):
     response = ""
-    while True:
+    while response == "" or response[len(response) - 1] != "\n":
         packet = sock.recv(8192)
-        response = response + packet.decode('utf-8')
-        try:
-            json.loads(response)
-            break
-        except:
-            pass
+        response += packet.decode('utf-8')
+    try:
+        json.loads(response)
+    except:
+        print("Message from server is invalid JSON")
+        exit(1)
     return response
 
-# def read_json_input():
-    # user_input = ""
-#     for line in sys.stdin:
-#         if :
-            
-#             break
-#     return user_input
+def validate_json_input(json_input):
+    try:
+        json.loads(json_input.strip())
+    except:
+        print('{"error" : "not a request", "object" : <JSON> }')
+        json_input = ""
+    return json_input
 
 # Transform the user input for network creation
 # into a valid create command to send to the server.
@@ -48,7 +48,7 @@ def transform_create_cmd(cmd):
             output["towns"].append(road["to"])
         if road["from"] not in output["towns"]:
             output["towns"].append(road["from"])
-    return output
+    return json.dumps(output)
 
 def transform_batch_cmds(cmds):
     # should already be list of jsons
@@ -61,11 +61,49 @@ def transform_batch_cmds(cmds):
             batch_req["characters"].append( \
                 {"name": cmd_params["name"], \
                     "town": cmd_params["town"]})
-        elif cmd_type = "passage-safe?":
+        elif cmd_type == "passage-safe?":
             batch_req["query"]["character"] = cmd_params["character"]
             batch_req["query"]["destination"] = cmd_params["town"]
-    return batch_req
+    return json.dumps(batch_req)
 
+def process_response(response, character_request):
+    response_json = json.loads(response)
+    for invalid_item in response_json["invalid"]:
+        print('["invalid placement", ' + json.dumps(invalid_item) + ']')
+    
+    request_json = json.loads(character_request)
+    print('["the response for", { "character" : ' + request_json["character"] + \
+         ', "destination" : ' + request_json["town"] + '} , "is", ' + \
+             json.dumps(response["response"]))
+
+def startup(sock):
+    sock.sendall(username.encode())
+    session_id = recieve(sock)
+    print('["the server will call me", ' + session_id + ']')
+    print("Please enter JSON for road network:")
+    create_cmd = ""
+    while create_cmd == "":
+        json_input = sys.stdin.readline()
+        create_cmd = validate_json_input(json_input)
+    create_cmd = transform_create_cmd(create_cmd)
+    sock.sendall(create_cmd.encode())
+
+def process(sock):
+    batch = []
+    for line in sys.stdin:
+        cmd = ""
+        while cmd == "":
+            cmd = validate_json_input(line)
+        batch.append(json.loads(cmd))
+
+        if json.loads(cmd)["command"] == "passage-safe?":
+            sock.sendall(transform_batch_cmds(batch).encode())
+            response = recieve(sock)
+            process_response(response, cmd)
+            batch = []
+
+def shutdown(sock):
+    sock.close()
 
 # open a server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -76,21 +114,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         print("Failed to connect to the server")
         exit(1)
 
-    # startup steps
-    sock.sendall(username.encode())
-    session_id = recieve(sock)
-    # recieve network spec from user
-    # send create request to server
-
-    # processing phase
-    while True:
-        print("Enter JSON input:")
-        json_input = read_json_input()
-        sock.sendall(json_input.encode())
-
-        # keep reading from server until a newline character is found
-        response = recieve(sock)
-        print(response)
-
-    # shutdown steps
-    
+    # run the three phases of the program
+    # (startup steps, processing phase, shutdown steps)
+    startup(sock)
+    process(sock)
+    shutdown(sock)
