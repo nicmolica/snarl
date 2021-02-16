1d28ea4e2f182cdaa20e2e69ca3091999fb31bb9
nichols-t <tynichols0@gmail.com>
2021-02-16 16:15:58 -0500

    Removed unnecessary directory

--- src/test_numjson.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestNumJSON(unittest.TestCase):
-    def test_mult(self):
-        self.assertEqual(mult([3, 6, 9]), 162, "Should be 162")
-
-if __name__ == '__main__':
-    unittest.main()
