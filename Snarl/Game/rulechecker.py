b7d8cc6d89601dafde20273ef3ca592ded9c1b5e
nmolica <60078553+nmolica@users.noreply.github.com>
2021-02-19 17:06:25 -0500

    Updating rulechecker

--- Snarl/Game/rulechecker.py
+++ Snarl/Game/rulechecker.py
@@ -3,18 +3,28 @@ from level import Level
 
 class Rulechecker:
     def is_valid_player_move(self, player, src, dest, current_level):
+        """ Is moving the player from src to dest a valid move on the provided level?
+        """
         # player is Occupant, src is Tile (or None if player isn't a Player/Adversary), dest is Tile, level is Level
         return True
 
     def is_valid_adversary_move(self, player, src, dest, current_level):
+        """ Is moving the adversary from src to dest a valid move on the provided level?
+        """
         # player is Occupant, src is Tile (or None if player isn't a Player/Adversary), dest is Tile, level is Level
         return True
 
     def is_level_over(self, level):
+        """ Has the given level been completed?
+        """
         return True
 
     def is_game_over(self, gamestate):
+        """ Has the game been completed?
+        """
         return True
 
     def did_players_win(self, gamestate):
+        """ Are there any players left alive? This method assumes the game is over.
+        """
         return gamestate.current_level.players != []
